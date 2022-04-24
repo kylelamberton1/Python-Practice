43fccb936784d87b9aa10a68720888a66ed81001
Susan Lamberton <kylelamberton1@gmail.com>
2022-04-24 21:47:54 +0100

    1st task

---
+++ name_generator.py
@@ -0,0 +1,4 @@
+print ("Welcome to the drag-queen name generator!")
+animal = input("What is the name of your first pet?\n")
+food = input("What is the name of your favourite snack?\n")
+print ("Your drag-queen name is Tobey" + animal + " " + food)
